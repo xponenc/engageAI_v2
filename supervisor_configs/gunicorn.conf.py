4179fb8bc750cd30120fea6293e2c06250531107
o.boykov <xponenc@gmail.com>
2025-11-27 17:45:13 +0400

    v2 supervisor config

---
+++ supervisor_configs/gunicorn.conf.py
@@ -0,0 +1,8 @@
+bind = '127.0.0.1:8000'  # Оставляем, если используете Nginx
+workers = 5              # Увеличиваем до 9 для лучшего использования CPU
+threads = 2              # Добавляем 2–4 потока для I/O-bound задач
+user = 'bo'              # Без изменений
+timeout = 120            # Без изменений или уменьшить до 90, если запросы быстрые
+loglevel = 'info'        # Добавляем для логирования
+accesslog = "/home/bo/projects/knowledge_base/knowledge_base/logs/gunicorn_access.log"
+errorlog = "/home/bo/projects/knowledge_base/knowledge_base/logs/gunicorn_error.log"
